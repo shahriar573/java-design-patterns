bc69d19ab6b6356386506508155fe960cce81bea
Shahriar Aziz Khan <khan2107034@stud.kuet.ac.bd>
2025-08-20 18:35:17 +0600

    Changes made

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,25 @@
-public class Calculator {
-    public int add(int a, int b) {
-        System.out.println("Adding numbers in MAIN branch");
-        return a + b;
+public class Computer
+{
+    public int add(int a, int b)
+    {
+        return a+b;
     }
 
-    public int subtract(int a, int b) {
-        System.out.println("Subtracting numbers in MAIN branch");
-        return a - b;
+    public int subtract(int a, int b)
+    {
+        return a-b;
     }
 
-    public int multiply(int a, int b) {
-        return a * b;
+    public int multiply(int a, int b)
+    {
+        return a*b;
     }
 
-    public int divide(int a, int b) {
-        return a / b;
+    public int divide(int a, int b)
+    {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a/b;
     }
 }
